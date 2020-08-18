6e3eb6d7351e75a8843fcede1aad3c56957e18bd
Gian Alves <gian.quinhones@gmail.com>
2020-08-18 14:35:40 -0300

    Redirecionamento depois do login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,16 @@ import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ SignInComponent ],
-    imports: [ CommonModule, ReactiveFormsModule, VmessageModule ]
+    imports: [ 
+        CommonModule, 
+        ReactiveFormsModule, 
+        VmessageModule, 
+        RouterModule
+    ]
 })
 
 export class HomeModule {

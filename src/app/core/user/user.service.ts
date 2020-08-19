0630d0a42c54d6d1159fac50496eda936fa2457f
Gian Alves <gian.quinhones@gmail.com>
2020-08-19 13:38:41 -0300

    AuthGuard -> Guarda de rotas

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -9,6 +9,7 @@ export class UserService {
 
     // private userSubject = new Subject<User>(); // Subject não aguarda o header ser carregado e assim perde as informações.
     private userSubject = new BehaviorSubject<User>(null); // BehaviorSubject irá aguardar o header ser carregado para mandar as informações.
+    private userName: string;
     
     constructor(private tokenService: TokenService) {
 
@@ -28,11 +29,20 @@ export class UserService {
     private decodeAndNotify() {
         const token = this.tokenService.getToken();
         const user = jtw_decode(token) as User;
+        this.userName = user.name;
         this.userSubject.next(user);
     }
 
     logout() {
         this.tokenService.removeToken();
         this.userSubject.next(null)
+    }
+
+    isLogged() {
+        return this.tokenService.hasToken();
+    }
+
+    getUserName() {
+        return this.userName;
     }
 }

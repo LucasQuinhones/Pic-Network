79c0f83c6894c8482a0bd50346b1e58ba2cc924d
Gian Alves <gian.quinhones@gmail.com>
2020-08-20 13:46:12 -0300

    Criando o próprio validador

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({ templateUrl: './signup.component.html'})
 
@@ -27,7 +28,7 @@ export class SignupComponent implements OnInit{
             userName: ['',
                 [
                     Validators.required,
-                    Validators.pattern(/^[a-z0-9_\-]+$/),
+                    LowerCaseValidator,
                     Validators.minLength(2),
                     Validators.maxLength(30)
                 ]
